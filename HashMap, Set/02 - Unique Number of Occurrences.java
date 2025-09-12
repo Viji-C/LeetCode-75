aaa842a39ca21f9854189e0abfe1865898953a4b
VIJI C <vijichinnaraj2001@gmail.com>
2025-09-12 22:41:31 +0530

    Unique Number of Occurrences

---
+++ HashMap, Set/02 - Unique Number of Occurrences.java
@@ -0,0 +1,44 @@
+/** 1207. Unique Number of Occurrences - https://leetcode.com/problems/unique-number-of-occurrences
+
+Given an array of integers arr, return true if the number of occurrences of each value in the array is unique or false otherwise.
+
+ 
+
+Example 1:
+
+Input: arr = [1,2,2,1,1,3]
+Output: true
+Explanation: The value 1 has 3 occurrences, 2 has 2 and 3 has 1. No two values have the same number of occurrences.
+Example 2:
+
+Input: arr = [1,2]
+Output: false
+Example 3:
+
+Input: arr = [-3,0,1,-3,1,1,1,-3,10,0]
+Output: true
+ 
+
+Constraints:
+
+1 <= arr.length <= 1000
+-1000 <= arr[i] <= 1000 **/
+
+// Solution
+class Solution {
+    public boolean uniqueOccurrences(int[] arr) {
+        int len = arr.length;
+        Map<Integer, Integer> freq = new HashMap<>();
+        for(int i=0;i<len;i++)
+        {
+            freq.put(arr[i], freq.getOrDefault(arr[i], 0)+1);
+        }
+        Set<Integer> set = new HashSet<>();
+        for(int value: freq.values())
+        {
+            if(set.contains(value)) return false;
+            set.add(value);
+        }
+        return true;
+    }
+}
