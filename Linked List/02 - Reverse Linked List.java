e49c4fd12f0ea4c8e4b5adf3d8eb1f671da433c9
VIJI C <vijichinnaraj2001@gmail.com>
2025-09-15 22:24:28 +0530

    Reverse Linked List

--- Linked List/02 - Reverse Linked List.java
+++ Linked List/02 - Reverse Linked List.java
@@ -22,6 +22,42 @@ Constraints:
 The number of nodes in the list is the range [0, 5000].
 -5000 <= Node.val <= 5000 **/
 
+
+// Using Stack
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        Stack<ListNode> stack = new Stack<>();
+        ListNode temp = head;
+        while(temp!=null)
+        {
+            stack.push(temp);
+            temp = temp.next;
+        }
+        if(!stack.isEmpty())
+        {
+            head = stack.pop();
+            temp = head;
+            while(!stack.isEmpty())
+            {
+                temp.next = stack.pop();
+                temp = temp.next;
+            }
+            temp.next = null;
+        }
+        return head;
+    }
+}
+
 // Solution
 /**
  * Definition for singly-linked list.
